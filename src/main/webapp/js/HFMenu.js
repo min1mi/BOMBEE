313fffd725677245483d7d9b2c8eca591fb4dbc8
hsm0730 <hsm0730@naver.com>
2017-07-10 16:18:20 +0900

    .
    .

---
+++ src/main/webapp/js/HFMenu.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "66%";
+    document.getElementById("otherSidenav").style.width = "33%";
+
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("otherSidenav").style.width = "0%";
+}
