833c82d43e81d11cd1a59f20b95152ce52ce8863
min1mi <min2mi00@gmail.com>
2017-07-24 19:55:38 +0900

    user 달력 ㅠㅠ 감격

--- src/main/webapp/js/user.js
+++ src/main/webapp/js/user.js
@@ -1,70 +1,89 @@
 $("document").ready(function(){
   $(".tab-slider--body").hide();
   $(".tab-slider--body:first").show();
+
+  date(current)
+  moment().format();
+
+
+  $(".tab-slider--nav .tab-slider--tabs div").click(function() {
+    $(".tab-slider--body").hide();
+    var activeTab = $(this).attr("rel");
+    $("#"+activeTab).fadeIn();
+  	if($(this).attr("rel") == "monday"){
+  		$('.tab-slider--tabs').addClass('slide1');
+  	}else if($(this).attr("rel") == "tuesday"){
+  		$('.tab-slider--tabs').addClass('slide2');
+  	}else if($(this).attr("rel") == "wednesday"){
+  		$('.tab-slider--tabs').addClass('slide3');
+  	}else if($(this).attr("rel") == "thursday"){
+  		$('.tab-slider--tabs').addClass('slide4');
+  	}else if($(this).attr("rel") == "friday"){
+  		$('.tab-slider--tabs').addClass('slide5');
+  	}else if($(this).attr("rel") == "saturday"){
+  		$('.tab-slider--tabs').addClass('slide6');
+  	}
+  	else {
+  		$('.tab-slider--tabs')
+  			.removeClass('slide1 slide2 slide3 slide4 slide5 slide6');
+  	}
+    $(".tab-slider--nav .tab-slider--tabs div").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  // var swiper = new Swiper('.swiper-container', {
+  //       loop: true
+  //     }, swipeCalendar());
+  //
+  $('#btttn').on('click', function() {
+    swipeCalendar()
+  })
+
 });
 
-$(".tab-slider--nav .tab-slider--tabs div").click(function() {
-  $(".tab-slider--body").hide();
-  var activeTab = $(this).attr("rel");
-  $("#"+activeTab).fadeIn();
-	if($(this).attr("rel") == "monday"){
-		$('.tab-slider--tabs').addClass('slide1');
-	}else if($(this).attr("rel") == "tuesday"){
-		$('.tab-slider--tabs').addClass('slide2');
-	}else if($(this).attr("rel") == "wednesday"){
-		$('.tab-slider--tabs').addClass('slide3');
-	}else if($(this).attr("rel") == "thursday"){
-		$('.tab-slider--tabs').addClass('slide4');
-	}else if($(this).attr("rel") == "friday"){
-		$('.tab-slider--tabs').addClass('slide5');
-	}else if($(this).attr("rel") == "saturday"){
-		$('.tab-slider--tabs').addClass('slide6');
-	}
-	else {
-		$('.tab-slider--tabs')
-			.removeClass('slide1 slide2 slide3 slide4 slide5 slide6');
-	}
-  $(".tab-slider--nav .tab-slider--tabs div").removeClass("active");
-  $(this).addClass("active");
-});
+var months = ["January","February","March",
+              "April","May","June","July",
+              "August","September","October",
+              "November","December"],
+    current = moment(new Date());
 
-date();
+function date(moment) { // get current date
+ var sunDate = moment.clone(),
+     month = months[current.get('month')];
 
-function date() {
-  var current = new Date(), // get current date
-      year = current.getFullYear(),
-      months = ["January","February","March",
-                "April","May","June","July",
-                "August","September","October",
-                "November","December"],
-      month = months[current.getMonth()];
-  var weekstart = current.getDate() - current.getDay() + 1;
-  console.log(year, month);
-  $('.year').html(year);
+  sunDate.add('days', -moment.weekday())
+  console.log(sunDate.toDate());
+  // var tempDate = moment(current)
+
+  $('.year').html(current.get('year'));
   $('.month').html(month);
 
-  $('.sun').html(weekstart - 1);
-  $('.mon').html(weekstart);
-  $('.tue').html(weekstart + 2);
-  $('.wed').html(weekstart + 3);
-  $('.thu').html(weekstart + 4);
-  $('.fri').html(weekstart + 5);
-  $('.sat').html(weekstart + 6);
+for (var i = 0; i < 7; i++) {
+  $('.day' + i).html(sunDate.date())
+  sunDate.add('days', 1)
+}
 }
 
-$('#calendar').datepicker({
-  dayNames: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-  dayNamesMin: ["일", " 월", " 수", " 목", " 금", " 토", " 일"],
-  format: "mm-yyyy",
-  viewMode: "months",
-  minViewMode: "months",
-  showWeek: true,
-  onSelect: function(dateText, inst) {
-    var date = $(this).datepicker('getDate'),
-        day  = date.getDate(),
-        month = date.getMonth() + 1,
-        year =  date.getFullYear();
-    alert(day + '-' + month + '-' + year);
-  }
-});
-$("#calendar").datepicker("setDate", new Date());
+
+
+function swipeCalendar() {
+  date(current.weekday(7))
+}
+
+
+// $('#calendar').datepicker({
+//   dayNames: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+//   dayNamesMin: ["일", " 월", " 수", " 목", " 금", " 토", " 일"],
+//   format: "mm-yyyy",
+//   viewMode: "months",
+//   minViewMode: "months",
+//   showWeek: true,
+//   onSelect: function(dateText, inst) {
+//     var date = $(this).datepicker('getDate'),
+//         day  = date.getDate(),
+//         month = date.getMonth() + 1,
+//         year =  date.getFullYear();
+//     alert(day + '-' + month + '-' + year);
+//   }
+// });
+// $("#calendar").datepicker("setDate", new Date());
