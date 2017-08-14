f97ac83ea2fed27b76e11825c2f69acdced06859
seungyeol <yeol1432@hanmail.net>
2017-08-14 17:49:14 +0900

    월요일 승열일지
    1

--- src/main/webapp/js/t-detail1.js
+++ src/main/webapp/js/t-detail1.js
@@ -1,20 +1,8 @@
-var  mno
-var no
-var tno
 $.getJSON('/auth/userinfo.json', function(result) {
-	console.log(result)
-	mno = result.data.no
-	console.log(result.data.no)
-	no = 0
-	try {
-			no = location.href.split('?')[1].split('=')[1]
-	} catch (err) {
-		console.log(no)
-
-	}
+	// console.log(result)
+	no = result.data.no
   generateTemplate();
 	getData();
-
 })
 function generateTemplate() {
   $.getJSON('/trainer/detail.json', {no}, function(result) {
@@ -25,7 +13,6 @@ function generateTemplate() {
     var html = container.html()
     container.html(html + generatedHTML)
     console.log(result)
-		tno = result.data.no
     switch (result.data.spono){
       case "1" :
       $('.t-spono').text('요가')
@@ -42,7 +29,6 @@ function generateTemplate() {
       default :
       console.log("선택하세요")
     }
-		return tno;
   })
 }
 function getData() {
@@ -69,12 +55,4 @@ jQuery(document).ready(function($){
 $(".pro-t-Btn").click(function(){
 $(".t-t-table").slideToggle(400);
 });
-})
-
-$('.pro-add-Btn').on('click', function() {
-
-	console.log(mno)
-	console.log(tno)
-
-
 })
