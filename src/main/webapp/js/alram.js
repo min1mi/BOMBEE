6fdea2d489a29ca07b0329db099614e665706137
min1mi <min2mi00@gmail.com>
2017-08-23 21:34:17 +0900

    .

--- src/main/webapp/js/alram.js
+++ src/main/webapp/js/alram.js
@@ -1,23 +1,33 @@
 $(function() {
   $('.header').load('../menu/new.html')
 })
+var no = null;
 
-$.ajax({
-  url: 'http://localhost:8888/alert/get.json',
-  type: 'post',
-  data:{no: no},
-  dataType:'json',
-  success: function(result) {
-    console.log(result)
-      var templateFn = Handlebars.compile($('#requested-template').text())
-      var generatedHTML = templateFn(result.data) // 템플릿 함수에 데이터를 넣고 HTML을 생성한다.
-      var container = $('.requested-container')
-      container.html("")
-      container.html(generatedHTML)
-    if(result.length < 0) {
-    }
+$.getJSON('/auth/userinfo.json', function(result) {
+  console.log(result)
+  if (result.status != 'fail') {
+    no = result.data.no
+    
+    $.ajax({
+      url: 'http://' + location.host + ':8888/alert/get.json',
+      type: 'post',
+      data:{no: no},
+      dataType:'json',
+      success: function(result) {
+        console.log(result)
+//          var templateFn = Handlebars.compile($('#alram-template').text())
+//          var generatedHTML = templateFn(result.data) // 템플릿 함수에 데이터를 넣고 HTML을 생성한다.
+//          var container = $('.alram-container')
+//          container.html("")
+//          container.html(generatedHTML)
+        if(result.length < 0) {
+        }
+      }
+    })
   }
 })
+
+
       
 function btnConnect() {
   $('.cancle').click(function() {
