cb24658a0b20e810f6d7e401c93f317ddedf2852
kwonbongsoo <star1231076@gmail.com>
2017-08-25 23:29:38 +0900

    sadfasdf
    asdfasdfasfd

--- src/main/webapp/js/alram.js
+++ src/main/webapp/js/alram.js
@@ -2,31 +2,38 @@ $(function() {
   $('.header').load('../menu/new.html')
 })
 var no = null;
+var json = '/auth/userinfo.json'
 
-$.getJSON('/auth/userinfo.json', function(result) {
-  console.log(result)
-  if (result.status != 'fail') {
-    no = result.data.no
-    
-    $.ajax({
-      url: 'http://' + location.host + ':8888/alert/get.json',
-      type: 'post',
-      data:{no: no},
-      dataType:'json',
-      success: function(result) {
-        console.log(result)
-          var templateFn = Handlebars.compile($('#alram-template').text())
-          var generatedHTML = templateFn(result) // 템플릿 함수에 데이터를 넣고 HTML을 생성한다.
-          var container = $('.alram-info-container')
-          container.html("")
-          container.html(generatedHTML)
-          btnConnect()
-        if(result.length < 0) {
-        }
-      }
-    })
-  }
-})
+function getData(json, no) {
+	$.getJSON(json,{no}, function(result) {
+		  console.log(result)
+		  if(json == '/auth/userinfo.json') {
+			  if (result.status != 'fail') {
+				  no = result.data.no
+			  }
+		  }else if (json == '/friend/friendDelete.json') {
+			  location.reload()
+		  }
+		  
+		  $.ajax({
+			  url: 'http://' + location.host + ':8888'+json,
+			  type: 'post',
+			  data:{no: no},
+			  dataType:'json',
+			  success: function(result) {
+				  console.log(result)
+				  if(json == '/auth/userinfo.json'){
+					  var templateFn = Handlebars.compile($('#alram-template').text())
+					  var generatedHTML = templateFn(result) // 템플릿 함수에 데이터를 넣고 HTML을 생성한다.
+					  var container = $('.alram-info-container')
+					  container.html("")
+					  container.html(generatedHTML)
+				  }
+				  btnConnect()
+			  }
+		  })
+		})
+}
 
   
 function btnConnect() {
