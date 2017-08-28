6903e3e759f2d6898056cb9a98231cc1fbabf61b
hsm0730 <hsm0730@naver.com>
2017-08-28 10:36:03 +0900

    스와이퍼 수정
    스와이퍼 수정

--- src/main/webapp/js/trainerDetail.js
+++ src/main/webapp/js/trainerDetail.js
@@ -431,7 +431,6 @@ var swiper = new Swiper('.swiper-container', {
         scrollbar: '.swiper-scrollbar',
         scrollbarHide: true,
         slidesPerView: 'auto',
-        centeredSlides: true,
         spaceBetween: 30,
         grabCursor: true,
         observer:true
